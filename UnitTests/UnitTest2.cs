4d6517f75862f5607ebc69656f7defeaf50ed949
Benjamin Liu Po-Ching <benjamin.liu@titansoft.com.sg>
2023-12-07 14:31:44 +0800

    init unit tests

---
+++ UnitTests/UnitTest2.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+
+namespace UnitTests;
+
+[TestFixture]
+public class OrderServiceTests
+{
+    [Test]
+    public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
+    {
+        // hard to isolate dependency to unit test
+
+        var target = new OrderService();
+        //target.SyncBookOrders();
+        Assert.Fail();
+    }
+}
+
+public class OrderService
+{
+    private string _filePath = @"C:\temp\testOrders.csv";
+
+    public void SyncBookOrders()
+    {
+        var orders = GetOrders();
+
+        // only get orders of book
+        var ordersOfBook = orders.Where(x => x.Type == "Book");
+
+        var bookDao = new BookDao();
+        foreach (var order in ordersOfBook)
+        {
+            bookDao.Insert(order);
+        }
+    }
+
+    private List<Order> GetOrders()
+    {
+        // parse csv file to get orders
+        var result = new List<Order>();
+
+        // directly depend on File I/O
+        using (StreamReader sr = new StreamReader(this._filePath, Encoding.UTF8))
+        {
+            int rowCount = 0;
+
+            while (sr.Peek() > -1)
+            {
+                rowCount++;
+
+                var content = sr.ReadLine();
+
+                // Skip CSV header line
+                if (rowCount > 1)
+                {
+                    string[] line = content.Trim().Split(',');
+
+                    result.Add(this.Mapping(line));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private Order Mapping(string[] line)
+    {
+        var result = new Order
+        {
+            ProductName = line[0],
+            Type = line[1],
+            Price = Convert.ToInt32(line[2]),
+            CustomerName = line[3]
+        };
+
+        return result;
+    }
+}
+
+public class Order
+{
+    public string Type { get; set; }
+
+    public int Price { get; set; }
+
+    public string ProductName { get; set; }
+
+    public string CustomerName { get; set; }
+}
+
+public class BookDao
+{
+    internal void Insert(Order order)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ UnitTests/UnitTest3.cs
@@ -0,0 +1,93 @@
+﻿namespace UnitTests;
+
+[TestFixture]
+public class AuthenticationServiceTests
+{
+    [Test]
+    public void isValid_true_log_should_not_execute()
+    {
+        var target = new AuthenticationService();
+
+        var actual = target.IsValid("titansoft", "10314159");
+
+        //always failed
+        Assert.IsTrue(actual);
+    }
+
+    [Test]
+    public void isValid_false_log_should_execute()
+    {
+        var target = new AuthenticationService();
+
+        var actual = target.IsValid("titansoft", "10314159");
+
+        //always failed
+        Assert.IsTrue(actual);
+    }
+}
+
+public class AuthenticationService
+{
+    public bool IsValid(string account, string passCode)
+    {
+        // 根據 account 取得自訂密碼
+        var profileDao = new ProfileDao();
+        var passwordFromDao = profileDao.GetPassword(account);
+
+        // 根據 account 取得 RSA token 目前的亂數
+        var rsaToken = new RsaTokenDao();
+        var randomCode = rsaToken.GetRandom(account);
+
+        // 驗證傳入的 passCode 是否等於自訂密碼 + RSA token亂數
+        var validPasscode = passwordFromDao + randomCode;
+        var isValid = passCode == validPasscode;
+
+        if (!isValid)
+        {
+            // todo, 如何驗證當有非法登入的情況發生時，有正確地記錄log？
+            var content = string.Format("account:{0} try to login failed", account);
+            Console.Write(content);
+        }
+
+        return isValid;
+
+    }
+}
+
+public class ProfileDao
+{
+    public string GetPassword(string account)
+    {
+        return Context.GetPassword(account);
+    }
+}
+
+public static class Context
+{
+    public static Dictionary<string, string> profiles;
+
+    static Context()
+    {
+        profiles = new Dictionary<string, string>();
+        profiles.Add("titansoft", "10");
+    }
+
+    public static string GetPassword(string key)
+    {
+        return profiles[key];
+    }
+}
+
+public class RsaTokenDao
+{
+    public string GetRandom(string account)
+    {
+        var seed = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+        var result = seed.Next(0, 999999).ToString("000000");
+        Console.WriteLine("randomCode:{0}", result);
+
+        return result;
+    }
+}
+
+
